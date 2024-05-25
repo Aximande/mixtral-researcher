a9f702f75e8a5a9166fdc530591c97c023563142
SadikhovEmin <eemin.sadykhov@gmail.com>
2024-05-25 16:04:27 +0200

    update path

--- pages/08_gpt_researcher_v2.py
+++ pages/08_gpt_researcher_v2.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import os
 import validators
 
-sys.path.append("../gpt_researcher_local")
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'gpt_researcher_local')))
 from gpt_researcher.master.agent import GPTResearcher
 
 
